5a6a63e1f4bffb4a2f63e4b00f7a30ea157ceee5
不叫耶稣的上帝 <57082321+2694359979@users.noreply.github.com>
2021-03-28 11:54:36 +0800

    Update rm_kalamnfilter.h

--- rm_kalamnfilter.h
+++ rm_kalamnfilter.h
@@ -1,6 +1,6 @@
 #ifndef KALMANTEST_H
 #define KALMANTEST_H
-
+#define A 0.000001f
 #include<opencv2/opencv.hpp>
 #include<opencv2/core/core.hpp>
 #include<opencv2/highgui/highgui.hpp>
@@ -9,28 +9,25 @@
 #include<iostream>
 using namespace cv;
 using namespace std;
-#define ANTI_RANGE 1.01//指数增长的底数
-#define A 1.0e-8//给加速度a一个限定值(-A,A)之间
-#define MNC 1e-10//测量协方差矩阵R，更大会有更慢的回归
-#define DEAD_BAND 0
-#define SIZE_X 960
-#define SIZE_Y 480
+
 class RM_kalmanfilter
 {
 public:
     RM_kalmanfilter();
     ~RM_kalmanfilter();
-    Point2f point_Predict(double g_runtime,Point2d current_point);
+    Point2f point_predict(double _t1, Point2d _p1);
+    void reset();
+    int n=2;
+    float anti_range = 1.5;
 
 private:
-    Mat measurement_img;//测量矩阵
-    cv::KalmanFilter kalman_img;
+    cv::KalmanFilter S_kalman;
+    Mat measurement_img;
     Point2f last_point;
-//    Point2f last_predic_point;
-    double runtime=(1e-2) + 0.005666666f;
     double last_v=0;
-    int n=2;
-
+    double t =(1e-2) + 0.005666666f;
+    float x = 640;
+    float y = 400;
 };
 
-#endif // KALMANTEST_H
+#endif 
