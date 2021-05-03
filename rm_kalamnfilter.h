34040886dd2dee42797c98e894c678ed2a32b5c1
不叫耶稣的上帝 <57082321+robomaster-shl@users.noreply.github.com>
2021-05-03 19:06:29 +0800

    Update rm_kalamnfilter.h

--- rm_kalamnfilter.h
+++ rm_kalamnfilter.h
@@ -1,14 +1,3 @@
-#ifndef KALMANTEST_H
-#define KALMANTEST_H
-#define A 0.000001f
-#include<opencv2/opencv.hpp>
-#include<opencv2/core/core.hpp>
-#include<opencv2/highgui/highgui.hpp>
-#include<opencv2/imgproc/imgproc.hpp>
-#include <math.h>
-#include<iostream>
-using namespace cv;
-using namespace std;
 
 class RM_kalmanfilter
 {
@@ -30,4 +19,30 @@ private:
     float y = 400;
 };
 
+
+class Kalman1
+{
+public:
+    Kalman1(){
+        Q_ = 0.03f;
+        R_ = 0.05f;
+        t_ = 1.0f;
+        x_ = 0.0f;
+        p_ = 0.01f;
+    }
+    Kalman1(float Q, float R, float t, float x0, float p0){
+        Q_ = Q;
+        R_ = R;
+        t_ = t;
+        x_ = x0;
+        p_ = p0;
+    }
+        float run(float data){
+       x_pre_ = x_;                                      //x(k|k-1) = AX(k-1|k-1)+BU(k)
+        p_pre_ = p_ + Q_;                              //p(k|k-1) = Ap(k-1|k-1)A'+Q
+        kg_ = p_pre_ / (p_pre_ + R_);               //kg(k) = p(k|k-1)H'/(Hp(k|k-1)'+R)
+        x_ = x_pre_ + kg_ * (data - x_pre_);          //x(k|k) = X(k|k-1)+kg(k)(Z(k)-HX(k|k-1))
+        p_ = (1 - kg_) * p_pre_;                   //p(k|k) = (I-kg(k)H)P(k|k-1)
+        return x_;
+    }
 #endif 
